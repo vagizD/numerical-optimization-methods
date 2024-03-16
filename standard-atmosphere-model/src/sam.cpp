f9219d78d173805d3e839553774fc2c5fdd27aff
ZaharKondaurov <stallmens@yandex.ru>
2024-03-16 17:56:59 +0300

    aerodynamic drag force, test and compute the RHS of ODE

--- standard-atmosphere-model/src/sam.cpp
+++ standard-atmosphere-model/src/sam.cpp
@@ -1,14 +1,16 @@
 #include "sam.h"
-#include "constants.h"
-
 #include <cassert>
 #include <cmath>
+#include "constants.h"
 
 Pressure::Pressure(double p0, double t0) {
     t[0] = t0, p[0] = p0;
     for (int i = 1; i < 4; ++i) {
         t[i] = t[i - 1] - r[i - 1] * (h[i] - h[i - 1]);
-        p[i] = p[i - 1] * std::exp(g / (R * r[i - 1]) * std::log(1 - r[i - 1] * (h[i] - h[i - 1]) / t[i - 1]));
+        p[i] = p[i - 1] * std::exp(
+                              g / (R * r[i - 1]) *
+                              std::log(1 - r[i - 1] * (h[i] - h[i - 1]) / t[i - 1])
+                          );
     }
 }
 
@@ -27,7 +29,8 @@ int getLayer(double height) {
 double Pressure::operator()(double height) {
     int l = getLayer(height);
     if (r[l] != 0) {
-        return p[l] * std::exp(g / (R * r[l]) * std::log(1 - r[l] * (height - h[l]) / t[l]));
+        return p[l] *
+               std::exp(g / (R * r[l]) * std::log(1 - r[l] * (height - h[l]) / t[l]));
     }
     return p[l] * std::exp(g * (h[l] - height) / (R * t[l]));
 }
@@ -49,4 +52,25 @@ double DragCoef::operator()(double mach) {
 
     int i = static_cast<int>(mach / step);
     return coefs[i] + ((coefs[i + 1] - coefs[i]) / step) * (mach - i * step);
+}
+
+double
+AerodynamicDragForce::operator()(double mach, double y, double velocity, double s) {
+    return cd(mach) * density(y) * s * velocity * velocity / 2;
+}
+
+void nextState(
+    AerodynamicDragForce q,
+    double mass,
+    double mach,
+    double s,
+    const std::array<double, 4> &u,
+    std::array<double, 4> &result
+) {
+    double velocity = std::sqrt(u[1] * u[1] + u[3] * u[3]);
+    double qdf = q(mach, u[2], velocity, s);
+    result[0] = u[1];
+    result[1] = -(qdf * u[1]) / (mass * velocity);
+    result[2] = u[3];
+    result[3] = -(qdf * u[3]) / (mass * velocity) - g;
 }
